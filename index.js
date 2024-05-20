e7aba368500d237ea38d09ec4cc872ed60d43db7
Md. Mamun Or Rashid <mamun.rashid@infolytx.com>
2024-05-20 18:04:23 +0600

    Javascript Principles - Thread of Execution - Functions - Call Stack

---
+++ index.js
@@ -0,0 +1,10 @@
+//
+
+const num = 3;
+function multipleBy2(inputNumber) {
+    const result = inputNumber * 2;
+    return result;
+}
+
+const output = multipleBy2(num);
+const newOutput = multipleBy2(10);
